2edea4cb866abdaa0e9c4653ca2f924088dfc845
Mike Szczys <szczys@hotmail.com>
2016-07-10 16:56:08 +0000

    Socket server now runs as a daemon

--- chip44780daemon.py
+++ chip44780daemon.py
@@ -1,6 +1,15 @@
 import socket
 from threading import Thread
 import chip44780
+import daemon
+
+## Dependencies:
+# sudo pip install python-daemon
+
+## Socket example used in this script:
+# http://stackoverflow.com/a/23149098
+## Daemon example
+# http://stackoverflow.com/a/8375012
 
 MAX_LENGTH = 4096
 
@@ -15,16 +24,24 @@ def handle(clientsocket):
         chip44780.scrollMsg(buf,.35)
         chip44780.backlight(False)
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def launchSocket():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-PORT = 10000
-HOST = '127.0.0.1'
+    PORT = 10000
+    HOST = '127.0.0.1'
 
-serversocket.bind((HOST,PORT))
-serversocket.listen(10)
+    serversocket.bind((HOST,PORT))
+    serversocket.listen(10)
 
-while 1:
-    (clientsocket, address) = serversocket.accept()
+    while 1:
+        (clientsocket, address) = serversocket.accept()
 
-    ct = Thread(target=handle, args=(clientsocket,))
-    ct.run()
+        ct = Thread(target=handle, args=(clientsocket,))
+        ct.run()
+
+def run():
+    with daemon.DaemonContext():
+        launchSocket()
+
+if __name__ == "__main__":
+    run()
